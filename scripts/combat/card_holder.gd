e2a132f9b09db783e9a2ec2dc85cc406fb3bc10c
mannireis <90857553+mannireis@users.noreply.github.com>
2026-09-17 14:40:20 +0100

    More card stuff + Snapping

---
+++ scripts/combat/card_holder.gd
@@ -0,0 +1,15 @@
+extends Node
+
+@export var snap_points: Array[Control] = []
+var occupied: Dictionary = {} 
+
+func get_nearest_point(global_pos: Vector2) -> Control:
+	var nearest: Control = null
+	var nearset_dist := INF
+	for point in snap_points:
+		var point_center = point.global_position + point.size / 2
+		var dist = global_pos.distance_squared_to(point_center)
+		if dist < nearset_dist:
+			nearset_dist = dist
+			nearest = point
+	return nearest
--- scripts/combat/visual_card.gd
+++ scripts/combat/visual_card.gd
@@ -6,24 +6,69 @@ extends Control
 @export var card: CardBase
 
 var dragging = false
+var hovering = false
 var drag_offset = Vector2()
+var current_point: Control = null
+
+
+func _ready() -> void:
+	card_texture.texture = card.card_texture
+	pivot_offset = size / 2
+
 
 func _process(delta: float) -> void:
-	card_texture.texture = card.card_texture
+	animate_hovering()
 
 
 func _gui_input(event) -> void:
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		dragging = event.pressed
-		if dragging:
+		if event.pressed:
+			dragging = true
 			drag_offset = get_global_mouse_position() - global_position
+			if current_point:
+				var point_manager = get_parent()
+				if point_manager.occupied.get(current_point) == self:
+					point_manager.occupied.erase(current_point)
+				current_point = null
+		else:
+			dragging = false
+			snap_to_nearest_point()
 	elif event is InputEventMouseMotion and dragging:
 		global_position = get_global_mouse_position() - drag_offset
 
 
+func snap_to_nearest_point() -> void:
+	var point_manager = get_parent()
+	var card_center = global_position + size * scale / 2.0
+	var target_point = point_manager.get_nearest_point(card_center)
+	if target_point:
+		if point_manager.occupied.get(target_point) == null:
+			hovering = false
+			scale = Vector2.ONE
+			rotation_degrees = 0.0
+			var tween = create_tween()
+			tween.tween_property(self, "global_position", target_point.global_position + (target_point.size - size) / 2.0, 0.15)
+			point_manager.occupied[target_point] = self
+			current_point = target_point
+		else:
+			current_point = null
+
+
+func animate_hovering() -> void:
+	if dragging:
+		return
+	var tween = create_tween()
+	if hovering:
+		tween.tween_property(self, "scale", Vector2(1.2,1.2), 0.15)
+		tween.parallel().tween_property(self, "rotation_degrees", 15.0, 0.15)
+	else:
+		tween.tween_property(self, "scale", Vector2(1.0,1.0), 0.1)
+		tween.parallel().tween_property(self, "rotation_degrees", 0.0, 0.15)
+
+
 func _on_mouse_entered() -> void:
-	scale = Vector2(1.2,1.2)
+	hovering = true
 
 
 func _on_mouse_exited() -> void:
-	scale = Vector2(1,1)
+	hovering = false
